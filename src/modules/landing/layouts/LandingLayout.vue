9370ed06413caee41de963819a032d9529bb99da
CasJuan <castrojuanig25@gmail.com>
2025-02-04 14:12:30 -0300

    [add] 404 not found

--- src/modules/landing/layouts/LandingLayout.vue
+++ src/modules/landing/layouts/LandingLayout.vue
@@ -7,11 +7,17 @@
           <img alt="Vue logo" class="logo" src="../src/assets/logo.svg" width="40" height="40" />
         </a>
       </div>
-      <nav class="ml-auto space-x-4 flex items-center h-10 sm:space-x-6">
-        <RouterLink to="/"> Home </RouterLink>
-        <RouterLink to="/features"> Features </RouterLink>
-        <RouterLink to="/pricing"> Pricing </RouterLink>
-        <RouterLink to="/contact"> Contact </RouterLink>
+      <nav class="ml-auto space-x-4 flex flex-col items-end h-10 sm:space-x-6">
+        <div class="space-x-4">
+          <RouterLink :to="{ name: 'home' }"> Home </RouterLink>
+          <RouterLink to="/features"> Features </RouterLink>
+          <RouterLink to="/pricing"> Pricing </RouterLink>
+          <RouterLink to="/contact"> Contact </RouterLink>
+        </div>
+
+        <div class="space-x-4">
+          <RouterLink to="/login"> Login </RouterLink>
+        </div>
       </nav>
     </header>
     <!-- Fin Header -->
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,4 @@
+import NotFound404 from '@/modules/common/pages/NotFound404.vue'
 import HomePage from '@/modules/landing/pages/HomePage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
@@ -38,16 +39,21 @@ const router = createRouter({
       component: () => import('../modules/auth/layouts/AuthLayout.vue'),
       children: [
         {
-          path: '/login',
+          path: 'login',
           name: 'login',
           component: () => import('../modules/auth/pages/LoginPage.vue'),
         },
         {
-          path: '/register',
+          path: 'register',
           name: 'register',
           component: () => import('../modules/auth/pages/RegisterPage.vue'),
         },
       ],
+    },
+    //not found
+    {
+      path: '/:pathMatch(.*)*',
+      component: NotFound404,
     },
   ],
 })
