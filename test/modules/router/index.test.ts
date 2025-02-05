1f124a54b3462cf58f4e953b2a04cf4b5bdd7b3a
CasJuan <castrojuanig25@gmail.com>
2025-02-05 18:29:57 -0300

    [add] Pruebas en el router - Parte 2 - Argumentos

--- test/modules/router/index.test.ts
+++ test/modules/router/index.test.ts
@@ -2,6 +2,7 @@ import { mount } from '@vue/test-utils'
 import { describe } from 'node:test'
 import test from 'node:test'
 import router from '../../../src/router/index'
+import { RouteLocationNormalized } from 'vue-router'
 
 describe('Router', () => {
   const wrapper = mount(App, {
@@ -30,5 +31,29 @@ describe('Router', () => {
     await router.replace('/contact')
     await router.isReady()
     await router.push({ name: 'contact' })
+  })
+  test('should render loginPage pok emon', async () => {
+    await router.replace('/pokemon/151')
+    await router.isReady()
+
+    expect(wrapper.find('h1').text()).toContain('Login')
+  })
+  test('should render loginPage pok emon', async () => {
+    localStorage.setItem('userId', 'ABBC-123')
+
+    await router.replace('/pokemon/151')
+    await router.isReady()
+
+    expect(wrapper.find('h1').text()).toContain('Pokemon 151')
+    expect(wrapper.html()).toContain('')
+  })
+
+  test('should convert the segment into numbewr', () => {
+    const routes: RouteLocationNormalized = {}
+    const pokemonRoute = router.getRoutes().find((route) => route.name === 'pokemon')
+    const props = (pokemonRoute?.props as any).default(route)
+
+    expectd(pokemonRoute).toBeTruthy()
+    expect()
   })
 })
